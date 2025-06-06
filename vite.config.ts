a6d0f57fc7445c78ef43cfad3d66902f099f58c3
AniketJha200 <aniketjha2020@gmail.com>
2025-06-06 12:29:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import ghPages from "vite-plugin-gh-pages";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/aniket-jha-folio-forge/", // 👈 Add your repo name here
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    ghPages(),                      // 👈 Include ghPages plugin here
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
@@ -20,3 +22,4 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }));
+
